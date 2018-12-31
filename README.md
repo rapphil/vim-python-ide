7449030162776a29b575a6d80c48b64cd06f5791
Raphael Silva <rapphil@gmail.com>
2018-12-30 17:13:55 -0800

    Feat: Add first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Vim as a Python IDE
+
+This repo contains a script to build vim from the source, and install a set
+of plugins that will make VIM look like Python IDE.
+
+Please check the available features and the plugins that enable them:
+
+## Syntax Highlighting
+
+## Color Themes
+
+## Project Navigation
+
+- Nerdtree
+- Tags
+- Fzf
+
+## Powerfull Full Text search
+
+- Ripgrep
+
+## Real time linting
+- Ale
+
+## Code navigation
+
+- guttentag + jedi
+
+## Code refactoring
+
+Jedi + Rope
+- Extract method
+- Rename functions
+
+## Code completion
+https://github.com/davidhalter/jedi-vim
+## Snippets
+
+Snippets save time while you type and create standardization
+
+## Git Integration
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+VIM_VERSION="v8.1.0664"
+
+CURDIR=${PWD}
+CPUS=$(grep -c ^processor /proc/cpuinfo)
+
+git clone --branch $VIM_VERSION https://github.com/vim/vim.git /tmp/vim-build/
+cd /tmp/vim-build
+
+./configure --prefix=${HOME}/tools/vim --with-features=huge --enable-multibyte --enable-pythoninterp=yes --enable-python3interp=yes
+
+make -j $CPUS && make install
+
+pip2 install --user rope jedi ropevim flake8
+
+cp ${CURDIR}/vimrc ${HOME}/.vimrc
+
+UPDATE_PATH='export PATH=${HOME}/tools/vim/bin:$PATH'
+export PATH=${HOME}/tools/vim/bin:$PATH
+
+if grep -qF "$UPDATE_PATH" ${HOME}/.bashrc; then
+	echo "Not updating bashrc"
+else
+	echo "$UPDATE_PATH"  >> ${HOME}/.bashrc
+fi
+
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+vim -u ${CURDIR}/plugins +PlugInstall! +qall!
+
+cat ${CURDIR}/plugins ${CURDIR}/configs > ${HOME}/.vimrc
