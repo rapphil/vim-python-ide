77dac0b2c31e407ca90d4cf703d7e0f9cc5a3ecc
Pyrrhu5 <67189832+Pyrrhu5@users.noreply.github.com>
2022-02-21 12:23:11 +0100

    No touchy my vimrc

--- install.sh
+++ install.sh
@@ -29,5 +29,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim -u ${CURDIR}/plugins +PlugInstall! +qall!
-
-cat ${CURDIR}/plugins ${CURDIR}/configs > ${HOME}/.vimrc
+# No touchy my vimrc
+# cat ${CURDIR}/plugins ${CURDIR}/configs > ${HOME}/.vimrc
